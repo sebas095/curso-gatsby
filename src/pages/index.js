6f5044cb2730ae3d3fbdafe1b6c2208f6971367c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-03-27 23:54:54 -0500

    [ADD] page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import Header from "../components/header"
+import Repos from "../components/repos"
+import Codigofacilito from "../components/codigofacilito"
+import Medium from "../components/medium"
+import EducationNav from "../components/education-nav"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Header />
+    <EducationNav />
+    <Repos />
+    <Codigofacilito />
+    <Medium />
+  </div>
+)
